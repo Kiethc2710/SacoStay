cb285f628b92b6f923223808937ee1231d91874b
Kiethc2710 <chikietsg@gmail.com>
2026-01-26 20:45:28 +0700

    Initial commit

---
+++ BE/SacoStayAPI/SacoStayAPI/Program.cs
@@ -0,0 +1,41 @@
+
+namespace SacoStayAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(C =>
+                {
+                    C.SwaggerEndpoint("/swagger/v1/swagger.json", "SacoStay API v1");
+                    C.RoutePrefix = "swagger";
+
+                });
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+
+                app.MapControllers();
+
+                app.Run();
+            }
+        }
+    }
+}
